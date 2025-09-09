260c9fe053d8ad72543ad44e6184b044fbf11aa4
Ryan Rocha <kayquesacanaje@gmail.com>
2025-09-08 21:09:09 -0300

    Drive.java

--- POO_Car/Drive.java
+++ POO_Car/Drive.java
@@ -1,7 +1,15 @@
+package POO_Car;
 public class Drive {
     
+   private static int speed;
+
     public static void Acelerar(Carro carro) {
         Main.print("Acelerando o " + carro.Modelo + " de " + carro.Ano);
+        speed = 5;
+    }
+
+    public static String GetSpeed() {
+        return String.valueOf(speed+" km/h");
     }
 
 }
